7f68212bb4366d46f6411a56dcd3d73164a3d815
Warren Voelkl <warren_voelkl@hotmail.com>
2011-03-08 15:53:09 -0800

    moving this from chatserver

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,104 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDateTime>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->sendButton, SIGNAL(clicked()), this, SLOT(sendMessage()));
+    enableChat(false);
+}
+
+void MainWindow::slotClientConnected(ClientConnect*) {
+
+}
+
+void MainWindow::slotTextRecieved(TextReceived*) {
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::sendMessage() {
+    QString message = ui->typeScreen->toPlainText();
+
+    if(message != "") {
+        printF(settings->alias + ": " + QTime::currentTime().toString()
+               + "\n" + message + "\n");
+    }
+
+    ui->typeScreen->clear();
+}
+
+void MainWindow::printF(const char *message) {
+    ui->chatScreen->appendPlainText(QString(*message));
+}
+
+void MainWindow::printF(QString message) {
+    ui->chatScreen->appendPlainText(message);
+}
+
+void MainWindow::on_actionExit_triggered() {
+    close();
+}
+
+void MainWindow::on_actionConnect_triggered() {
+    Settings *settingsDiag = new Settings(this);
+
+    settingsDiag->exec();
+
+    settings = settingsDiag->getSettings();
+
+    if(settingsDiag->result()) {
+        //TODO: Connect to server/Listen for clients
+        if(settings->isClient) {
+            //move these to be started when button pressed in gui
+            qDebug("Client");
+            setWindowTitle("Los Ostrich - Client");
+            enableChat(true);
+
+            qDebug(settings->ipAddr.toLatin1().data());
+            qDebug(QString::number(settings->port).toLatin1().data());
+            qDebug(settings->alias.toLatin1().data());
+
+            //TODO get a port from gui and ip just a hack here to make it compile
+            char *ip = settings->ipAddr.toLatin1().data();
+            TextClient * tc = new TextClient(ip, settings->port, BUFSIZE);
+            textClient = new Thread();
+            textClient->start();
+            connect(tc,SIGNAL(signalTextRecieved(TextReceived*)),
+                    this,SLOT(slotTextRecieved(TextReceived*)));
+            tc->moveToThread(textClient);
+            connect(this, SIGNAL(startSignalClient()), tc, SLOT(Start()));
+            emit startSignalClient();
+        } else {
+            qDebug("Server");
+            setWindowTitle("Los Ostrich - Server");
+            enableChat(false);
+
+            qDebug(QString::number(settings->port).toLatin1().data());
+
+            TextServer * ts = new TextServer(settings->port, BUFSIZE);
+            textServer = new Thread();
+            textServer->start();
+            connect(ts,SIGNAL(signalClientConnected(ClientConnect*)),
+                    this,SLOT(slotClientConnected(ClientConnect*)));
+            ts->moveToThread(textServer);
+            connect(this, SIGNAL(startSignalServer()), ts, SLOT(Start()));
+            emit startSignalServer();
+        }
+    } else {
+        qDebug("Settings Cancelled");
+    }
+}
+
+void MainWindow::enableChat(bool enable) {
+    ui->sendButton->setEnabled(enable);
+    ui->typeScreen->setEnabled(enable);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QThread>
+#include "textClient.h"
+#include "textServer.h"
+#include "settings.h"
+#include "thread.h"
+
+#define BUFSIZE 1024
 
 namespace Ui {
     class MainWindow;
@@ -14,9 +21,28 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void printF(const char*);
+    void printF(QString);
+    void enableChat(bool);
 
 private:
     Ui::MainWindow *ui;
+    Thread * textClient;
+    Thread * textServer;
+    PSETTINGS settings;
+
+public slots:
+    void slotClientConnected(ClientConnect *);
+    void slotTextRecieved(TextReceived *);
+
+private slots:
+    void on_actionConnect_triggered();
+    void on_actionExit_triggered();
+    void sendMessage();
+
+signals:
+    void startSignalClient();
+    void startSignalServer();
 };
 
 #endif // MAINWINDOW_H
