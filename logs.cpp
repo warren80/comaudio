877e6b22463318e7a2834411ba25fb97a3196e5f
Karl Castillo <khcastillo@hotmail.com>
2011-03-22 17:37:58 -0700

    Logs

---
+++ logs.cpp
@@ -0,0 +1,31 @@
+#include "logs.h"
+#include <QDateTime>
+
+Logs::Logs(QString pathName, QString title):
+        log_(pathName)
+{
+    log_.open(QIODevice::WriteOnly);
+    log_.write(title.toLatin1());
+    log_.close();
+}
+
+void Logs::writeToLog(const char *error, int errNo) {
+    QString errorMesg(error);
+    errorMesg +=  " " + QDateTime::currentDateTime().toString();
+    log_.open(QIODevice::Append);
+    log_.write(errorMesg.toLatin1().data());
+    log_.write("Errno: " + errNo);
+    log_.close();
+}
+
+void Logs::writeToLog(const char *message) {
+    log_.open(QIODevice::Append);
+    log_.write(message);
+    log_.close();
+}
+
+void Logs::writeToLog(QString message) {
+    log_.open(QIODevice::Append);
+    log_.write(message.toLatin1());
+    log_.close();
+}
