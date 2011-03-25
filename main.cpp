334cd7bd60150c8a9f474a8ee722e9aeda94025a
Nick Huber <Nicholashuber@gmail.com>
2011-03-24 22:44:33 -0700

    moved WSAStartup to main.cpp, and added WSACleanup

--- main.cpp
+++ main.cpp
@@ -1,11 +1,30 @@
 #include <QtGui/QApplication>
+
+#ifdef _WIN32
+#include <winsock2.h>
+#endif
+
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
+
+#ifdef _WIN32
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2,2),&wsaData) != 0) {
+        return 1;
+    }
+#endif
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
 
-    return a.exec();
+    int retCode = a.exec();
+
+#ifdef _WIN32
+    WSACleanup();
+#endif
+
+    return retCode;
 }
