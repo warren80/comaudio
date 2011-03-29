a11853821176691210a1d5cb6c4b412f7b935812
Nick Huber <Nicholashuber@gmail.com>
2011-03-28 22:46:35 -0700

    added transmit function to component

--- component.h
+++ component.h
@@ -17,15 +17,25 @@ public:
     virtual ~Component() {};
 
     /**
-      New data to be processed by the component.
+      Newly received data to be processed by the component.
       Must be implemented by each component.
 
       @param data Received data.
-      @param length Length of received data
+      @param length Length of received data.
       @author Nick Huber
 
       */
     virtual void receiveData(char* data, int length) = 0;
+
+    /**
+      Transmit data to the other side.
+
+
+      @param data Data to transmit.
+      @param length Size of data.
+      @author Nick Huber
+      */
+    void transmitData(char* data, int length) { socket_.transmit(data, length); };
 
 private:
     ComponentType type_;
--- componentstream.cpp
+++ componentstream.cpp
@@ -7,4 +7,3 @@ ComponentStream::ComponentStream(Socket socket) : Component(kStream, socket) {
 void ComponentStream::receiveData(char *data, int length) {
     player_.appendBuffer(data, length);
 }
-
