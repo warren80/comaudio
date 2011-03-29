2d316ee38b30265ddb8f2de9c441e6f4426357e4
Nick Huber <Nicholashuber@gmail.com>
2011-03-28 22:33:56 -0700

    added newData(char*,int) method to stream

--- componentstream.cpp
+++ componentstream.cpp
@@ -3,3 +3,8 @@
 ComponentStream::ComponentStream(Socket socket) : Component(kStream, socket) {
 
 }
+
+void ComponentStream::newData(char *data, int length) {
+    player_.appendBuffer(data, length);
+}
+
--- componentstream.h
+++ componentstream.h
@@ -2,10 +2,14 @@
 #define COMPONENTSTREAM_H
 
 #include "component.h"
-
+#include "audioplayer.h"
 class ComponentStream : public Component {
 public:
     ComponentStream(Socket socket);
+
+    void newData(char *data, int length);
+private:
+    AudioPlayer player_;
 };
 
 #endif // COMPONENTSTREAM_H
