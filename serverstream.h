e0eb05686329787704502d8fbc791d88e5086234
Warren Voelkl <warren_voelkl@hotmail.com>
2011-04-01 14:18:44 -0800

    ome commenting done

--- serverstream.h
+++ serverstream.h
@@ -1,10 +1,8 @@
 #ifndef SERVERSTREAM_H
 #define SERVERSTREAM_H
 
-
 #include <QObject>
 #include "Thread.h"
-
 
 #include "componentstream.h"
 
@@ -13,8 +11,20 @@ Q_OBJECT
 private:
     QString fileNamePath_;
 public:
+    /**
+     * Constructs an object which will push the data of a song
+     * onto a multicast port
+     * @author Warren Voelkl
+     */
+
     explicit ServerStream(QString fileNamePath);
 public slots:
+
+    /**
+     * The worker part of this worker thread It sets up a multicast port
+     * pushes the data over the port then removes the port.
+     * @author Warren Voelkl
+     */
     void slotStartTransfer();
 signals:
     void signalTransferDone();
