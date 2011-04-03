1dd761605848a6b364fbf8c011d6a80fe932dd48
Nick Huber <Nicholashuber@gmail.com>
2011-04-03 15:45:11 -0700

    fixed uppercase on serverFileTransfer, deleted more sockets

--- serverfiletransfer.h
+++ serverfiletransfer.h
@@ -11,14 +11,18 @@
 #define FT_HEADER_SIZE (sizeof(int) + sizeof(ComponentType))
 #define FT_DATA_SIZE   (FT_PACKET_SIZE - FT_HEADER_SIZE)
 
-class serverFileTransfer : public QObject
+class ServerFileTransfer : public QObject
 {
     Q_OBJECT
 private:
     QString fileName_;
     Socket *socket_;
 public:
-    serverFileTransfer(QString, Socket *);
+    ServerFileTransfer(QString, Socket *);
+    ~ServerFileTransfer() {
+        delete socket_;
+    }
+
 public slots:
     /**
      * The worker part of this worker thread It sets up a multicast port
